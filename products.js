d39ecb22a72fa6e6360cb32dd31e922ccab02e53
Ramendra-sharan <113187412+Ramendra-sharan@users.noreply.github.com>
2022-09-11 22:20:11 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,74 @@
+const products = [
+    {
+      id: 0,
+      name: "GL-N292DDSY",
+      price: 24000,
+      instock: 100,
+      description:
+        "LG Double Door.",
+      imgSrc: "img/products/f1.jpg",
+    },
+    {
+      id: 1,
+      name: "GL-D201ASCY",
+      price: 17000,
+      instock: 43,
+      description:
+        "LG 4 star single door refrigerator",
+      imgSrc: "img/products/f2.jpg",
+    },
+    {
+      id: 2,
+      name: "Dual-inverter window Ac",
+      price: 50000,
+      instock: 10,
+      description:
+        "LG",
+      imgSrc: "img/products/f3.jpg",
+    },
+    {
+      id: 3,
+      name: "Antiviral split AC",
+      price: 38316,
+      instock: 5,
+      description:
+        "LG",
+      imgSrc: "img/products/f4.jpg",
+    },
+    {
+      id: 4,
+      name: "Semi-automatic Washing machine",
+      price: 14500,
+      instock: 4,
+      description:
+        "LG",
+      imgSrc: "img/products/f5.jpg",
+    },
+    {
+      id: 5,
+      name: "6 motion direct drive Washing machine",
+      price: 32500,
+      instock: 40,
+      description:
+        "LG",
+      imgSrc: "img/products/f6.jpg",
+    },
+    {
+      id: 6,
+      name: "LGLQ57 32",
+      price: 21000,
+      instock: 40,
+      description:
+        "LG",
+      imgSrc: "img/products/f7.jpg",
+    },
+    {
+      id: 7,
+      name: "LG OLED",
+      price: 35000,
+      instock: 40,
+      description:
+        "LG",
+      imgSrc: "img/products/f8.jpg",
+    },
+  ];
---
+++ productsNA.js
@@ -0,0 +1,146 @@
+const productsNA = [
+    {
+      id: 0,
+      name: "GL-N292DDSY",
+      price: 24000,
+      instock: 100,
+      description:
+        "LG Double Door.",
+      imgSrc: "img/products/f1.jpg",
+    },
+    {
+      id: 1,
+      name: " GL-D201ASCY",
+      price: 17000,
+      instock: 43,
+      description:
+        "LG 4 star single door refrigerator",
+      imgSrc: "img/products/f2.jpg",
+    },
+    {
+      id: 2,
+      name: "Dual-inverter window Ac",
+      price: 50000,
+      instock: 10,
+      description:
+        "LG",
+      imgSrc: "img/products/f3.jpg",
+    },
+    {
+      id: 3,
+      name: "Antiviral split AC",
+      price: 38316,
+      instock: 5,
+      description:
+        "LG",
+      imgSrc: "img/products/f4.jpg",
+    },
+    {
+      id: 4,
+      name: "Semi-automatic Washing machine",
+      price: 14500,
+      instock: 4,
+      description:
+        "LG",
+      imgSrc: "img/products/f5.jpg",
+    },
+    {
+      id: 5,
+      name: "6 motion direct drive Washing machine",
+      price: 32500,
+      instock: 40,
+      description:
+        "LG",
+      imgSrc: "img/products/f6.jpg",
+    },
+    {
+      id: 6,
+      name: "LGLQ57 32",
+      price: 21000,
+      instock: 40,
+      description:
+        "LG",
+      imgSrc: "img/products/f7.jpg",
+    },
+    {
+      id: 7,
+      name: "LG OLED",
+      price: 35000,
+      instock: 40,
+      description:
+        "LG",
+      imgSrc: "img/products/f8.jpg",
+    },
+    {
+      id: 8,
+      name: "Ceiling floral fan",
+      price: 785,
+      instock: 100,
+      description:
+        "DG.",
+      imgSrc: "img/products/n1.jpg",
+    },
+    {
+      id: 9,
+      name: "warmth with light combo",
+      price: 200,
+      instock: 43,
+      description:
+        "DG",
+      imgSrc: "img/products/n2.jpg",
+    },
+    {
+      id: 10,
+      name: "Chargeable LED",
+      price: 456,
+      instock: 10,
+      description:
+        "DG",
+      imgSrc: "img/products/n3.jpg",
+    },
+    {
+      id: 11,
+      name: "Pack of 10",
+      price: 780,
+      instock: 5,
+      description:
+        "DG",
+      imgSrc: "img/products/n4.jpg",
+    },
+    {
+      id: 12,
+      name: "Ceiling LED ",
+      price: 250,
+      instock: 4,
+      description:
+        "DG",
+      imgSrc: "img/products/n5.jpg",
+    },
+    {
+      id: 13,
+      name: "Multicolor LED",
+      price: 600,
+      instock: 40,
+      description:
+        "DG",
+      imgSrc: "img/products/n6.jpg",
+    },
+    {
+      id: 14,
+      name: "Music+ LED",
+      price: 300,
+      instock: 40,
+      description:
+        "DG",
+      imgSrc: "img/products/n7.jpg",
+    },
+    {
+      id: 15,
+      name: "LED Tube",
+      price: 230,
+      instock: 40,
+      description:
+        "DG",
+      imgSrc: "img/products/n8.jpg",
+    },
+  ];
